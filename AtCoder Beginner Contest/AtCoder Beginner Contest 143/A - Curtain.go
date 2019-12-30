b7f19b380969f0491e892ebb3009a2225bc35935
sakozo <youlianggit@gmail.com>
2019-12-30 23:10:12 +0900

    Aうめ

--- AtCoder Beginner Contest/AtCoder Beginner Contest 143/A - Curtain.go
+++ AtCoder Beginner Contest/AtCoder Beginner Contest 143/A - Curtain.go
@@ -1,16 +1,16 @@
 package main
- 
+
 import (
-    "fmt"
+	"fmt"
 )
- 
+
 func main() {
-  	var a,b int
-		fmt.Scan(&a)
-		fmt.Scan(&b)
-		ans := 0
-		if a > 2*b{
-			ans = a-2*b
-		}
-  	fmt.Println(ans)
+	var a, b int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	ans := 0
+	if a > 2*b {
+		ans = a - 2*b
+	}
+	fmt.Println(ans)
 }
